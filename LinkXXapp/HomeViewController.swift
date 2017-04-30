ac0bffad1e77a3bfc252fa35d796f36a4c7aa688
Harry Ng <poripori1030@gmail.com>
2017-04-30 22:45:49 +0800

    home view ui

--- LinkXXapp/HomeViewController.swift
+++ LinkXXapp/HomeViewController.swift
@@ -7,29 +7,47 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func followMeButton_TouchUpInside(_ sender: Any) {
+        SCLAlertView().showTitle(
+            "Coming Soon",
+            subTitle: "Feature",
+            duration: 0.0,
+            completeText: "OK",
+            style: .success,
+            colorStyle: 0xF26C57,
+            colorTextButton: 0xFFFFFF)
     }
-    */
-
+    
+    @IBAction func chatButton_TouchUpInside(_ sender: Any) {
+        SCLAlertView().showTitle(
+            "Coming Soon",
+            subTitle: "Feature",
+            duration: 0.0,
+            completeText: "OK",
+            style: .success,
+            colorStyle: 0xF26C57,
+            colorTextButton: 0xFFFFFF)
+    }
+    
+    @IBAction func panicButton_TouchUpInside(_ sender: Any) {
+        SCLAlertView().showTitle(
+            "Coming Soon",
+            subTitle: "Feature",
+            duration: 0.0,
+            completeText: "OK",
+            style: .success,
+            colorStyle: 0xF26C57,
+            colorTextButton: 0xFFFFFF)
+    }
 }
